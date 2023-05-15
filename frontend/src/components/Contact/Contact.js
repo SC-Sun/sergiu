1bf75beb58971ef3dd22aad0d3cc292e442d8a43
SC-Sun <shangchisun@gmail.com>
2023-05-15 18:35:44 +0200

    add correction on contact/js and css

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -4,8 +4,8 @@ import "./Contact.css";
 const contactMe = {
   text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo",
   email: "mailto:sergiu.@example.com",
-  fb: "https://www.google.com/",
-  IG: "https://www.google.com/",
+  fb: "https://www.facebook.com/sergiu.matis/",
+  IG: "https://www.instagram.com/sergiu.matis/?hl=de",
 };
 const Contact = () => {
   return (
@@ -13,23 +13,43 @@ const Contact = () => {
       <p> {contactMe.text}</p>
       <a href={contactMe.email} target="_blank" rel="noopener noreferrer">
         {" "}
-        E-Mail
+        <span>E</span>
+        <span>-</span>
+        <span>M</span>
+        <span>a</span>
+        <span>i</span>
+        <span classname="move">l</span>
       </a>
       <br />
       <br />
       <span>
         {" "}
-        <a href={contactMe.email} target="_blank" rel="noopener noreferrer">
+        <a href={contactMe.fb} target="_blank" rel="noopener noreferrer">
           {" "}
-          Facebook
+          <span>F</span>
+          <span>a</span>
+          <span>c</span>
+          <span>e</span>
+          <span>b</span>
+          <span>o</span>
+          <span>o</span>
+          <span classname="move">k</span>
         </a>{" "}
       </span>
-      <br />
+
       <span>
         {" "}
-        <a href={contactMe.email} target="_blank" rel="noopener noreferrer">
+        <a href={contactMe.IG} target="_blank" rel="noopener noreferrer">
           {" "}
-          Instagram
+          <span>I</span>
+          <span>n</span>
+          <span>s</span>
+          <span>t</span>
+          <span>a</span>
+          <span>g</span>
+          <span>r</span>
+          <span>a</span>
+          <span classname="move">m</span>
         </a>{" "}
       </span>
     </div>
